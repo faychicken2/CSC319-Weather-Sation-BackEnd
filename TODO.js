ac8c2b4e9e7e8e9671553dc4719909ce865e1c88
[Michael H] <mikey_hinsberger@yahoo.com>
2021-07-18 06:26:05 -0700

    added a TODO file

---
+++ TODO.js
@@ -0,0 +1,7 @@
+/*
+TODO:
+    add getDataByDate function that pulls data with in a date range
+    add getDataByStation function that pulls data that is from specific station 
+    add getDataByStationDate function that pulls data that is from a specific station with in a date range
+
+*/
