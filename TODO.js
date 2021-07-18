de069cd3c33793438f06d2f45d1b455968eafffa
[Michael H] <mikey_hinsberger@yahoo.com>
2021-07-18 06:34:04 -0700

    updated TODOs

--- TODO.js
+++ TODO.js
@@ -1,7 +1,19 @@
 /*
-TODO:
+TODO Code:
     add getDataByDate function that pulls data with in a date range
     add getDataByStation function that pulls data that is from specific station 
     add getDataByStationDate function that pulls data that is from a specific station with in a date range
+
+*/
+
+/*
+TODO Docs:
+    create a flowchart
+
+*/
+
+/*
+TODO admin:
+    
 
 */
